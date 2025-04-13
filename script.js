f51ca5d1a0cd7b485445c131eb4592b0dc69533b
mariia-ty <tymoshenkom@icloud.com>
2025-04-13 13:12:50 -0400

    added practice and a shortcut way to avoid getting element and using event listener

--- script.js
+++ script.js
@@ -64,9 +64,61 @@ document.getElementById("clear").addEventListener("click", () => {
 });
 */
 
+/*
 document.getElementById("click").addEventListener("click", () => {
     document.getElementById("count").textContent = "Wait..."; 
     setTimeout(() => {
         document.getElementById("count").textContent = "Ready!!";
     }, 5000);
 });
+*/
+
+// document.getElementById("btn").addEventListener("click", () => {
+//   setTimeout(() => {
+//     document.getElementById("show").textContent = "Hello";
+//   }, 3000);
+// });
+
+// let counter = 0;
+// setInterval(() => {
+//   counter++;
+//   document.getElementById("count").textContent = counter;
+// }, 1000);
+
+// setTimeout(() => {
+//   document.getElementById("box").style.display= "none";
+// }, 5000);
+
+// let counter = 0;
+// const interval = setInterval(() => {
+//   counter++;
+//   document.getElementById("count").textContent = counter;
+//   if (counter >= 5) clearInterval(interval);
+// }, 1000);
+
+// document.getElementById("btn").addEventListener("click", () => {
+//   setTimeout(() => {
+//     document.getElementById("btn").style.backgroundColor = "green";
+//   }, 2000)
+// })
+
+// setInterval(() => {
+//   document.getElementById("txt").textContent = "hello "
+// }, 2000);
+
+// let counter = 10;
+// const timer = setInterval(() => {
+//   counter--;
+//   document.getElementById("counter").textContent = counter;
+//   if (counter <= -1) {
+//     clearInterval(timer);
+//     document.getElementById("counter").textContent = "start";
+//   }
+// }, 1000);
+
+
+function showHoverText() {
+  setTimeout(() => {
+    document.getElementById("txt").textContent = "Hovered";
+  }, 1000);
+};
